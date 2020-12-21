81857ead396bf47cddcdb1ebd2e05b9d0ee2fdb2
ariedro <ariedro@gmail.com>
2020-12-21 02:05:59 -0300

    feat: Functional POC

--- sensumbot.go
+++ sensumbot.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/sonyarouje/simdb/db"
 )
 
 const TOKEN = "SECRET"
@@ -16,9 +17,9 @@ const BOT_ID = "SECRET"
 const START_COMMAND = "/start@sensum_bot"
 const CHAT_IDS_FILE = "./sensumbot_chat_ids"
 const SENSUM_URL = "https://sensum-server.herokuapp.com/api/sensations/letThemFlow"
+const POLL_TICK = 5
 
-var last_id = "0"
-var last_post_id = ""
+var lastPostedSensationId = ""
 
 type Sensation struct {
 	ID        string
@@ -27,6 +28,16 @@ type Sensation struct {
 	Likes     int
 	Dislikes  int
 	Timestamp time.Time
+}
+
+type Receiver struct {
+	ChatID int64
+}
+
+func (r Receiver) ID() (jsonField string, value interface{}) {
+	value = r.ChatID
+	jsonField = "chatid"
+	return
 }
 
 func getLastSensation() Sensation {
@@ -40,7 +51,6 @@ func getLastSensation() Sensation {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
-
 	body, err := ioutil.ReadAll(resp.Body)
 
 	var sensations []Sensation
@@ -51,27 +61,55 @@ func getLastSensation() Sensation {
 	return sensations[0]
 }
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI(TOKEN)
-	if err != nil {
-		log.Panic(err)
-	}
+func sensumPoll(bot *tgbotapi.BotAPI, dbDriver *db.Driver) {
+	c := time.Tick(POLL_TICK * time.Second)
+	for range c {
+		log.Println("Checking")
+		lastSensation := getLastSensation()
+		if lastSensation.ID != lastPostedSensationId {
+			lastPostedSensationId = lastSensation.ID
+			log.Println(lastSensation.Message)
 
+			var receivers []Receiver
+			dbDriver.Open(Receiver{}).Get().AsEntity(&receivers)
+
+			for _, receiver := range receivers {
+				msg := tgbotapi.NewMessage(receiver.ChatID, lastSensation.Message+"\n~ "+lastSensation.Author)
+				bot.Send(msg)
+			}
+		}
+	}
+}
+
+func telegramPoll(bot *tgbotapi.BotAPI, dbDriver *db.Driver) {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
-
-	getLastSensation()
+	updates, _ := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil || update.Message.Text != START_COMMAND {
 			continue
 		}
-		log.Println(update.Message.Text)
+		dbDriver.Insert(Receiver{ChatID: update.Message.Chat.ID})
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, getLastSensation().Message)
-
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Agrega2")
 		bot.Send(msg)
 	}
+}
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+	dbDriver, err := db.New("data")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Bot started")
+
+	go telegramPoll(bot, dbDriver)
+	sensumPoll(bot, dbDriver)
 }
